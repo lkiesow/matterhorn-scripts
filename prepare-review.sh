ca07562b577759fc84309fd4f7fcecf34800bb99
Lars Kiesow <lkiesow@uos.de>
2013-04-16 15:02:24 +0200

    installscript for the scripts

---
+++ prepare-review.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp etc/init.d/matterhorn           etc-init.d-matterhorn
+cp opt/matterhorn/etc/service.conf opt-matterhorn-etc-service.conf
+cp usr/sbin/matterhorn             usr-sbin-matterhorn
